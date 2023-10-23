8d5b86cce7241cd4710e16e6974e2733c4e77ff2
Tyuiu.DubrovinSN@outlook.com <stepan.09myromec@gmail.com>
2023-10-23 19:57:56 +0500

    Task6

---
+++ Tyuiu.DubrovinSN.Sprint3.Task6.V2.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.DubrovinSN.Sprint3.Task6.V2.Lib;
+
+namespace Tyuiu.DubrovinSN.Sprint3.Task6.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void GetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+            int startValue = 12;
+            int stopValue = 18;
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            Assert.AreEqual(29, res);
+        }
+    }
+}
