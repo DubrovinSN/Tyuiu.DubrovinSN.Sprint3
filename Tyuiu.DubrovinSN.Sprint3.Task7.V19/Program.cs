170b5ab43febe10da730da2cd0a57f4832f224d4
Tyuiu.DubrovinSN@outlook.com <stepan.09myromec@gmail.com>
2023-10-25 20:42:54 +0500

    Task7

---
+++ Tyuiu.DubrovinSN.Sprint3.Task7.V19/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.DubrovinSN.Sprint3.Task7.V19.Lib;
+
+namespace Tyuiu.DubrovinSN.Sprint3.Task7.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Дубровин С.Н | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры                                   *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Дубровин Степан Николаевич | ИИПб-23-3                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу занчений функции            *");
+            Console.WriteLine("* F(x) = (5x + 2,5)/(sin(x) - 2) + 2                                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Старт шага - " + startValue);
+            Console.WriteLine("Конец шага - " + stopValue);
+            int len = ds.GetMassFunction(startValue,stopValue).Length;
+            double[] valueArray;
+            valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("+----------+------------+");
+            Console.WriteLine("|    X     |    F(x)    +");
+            Console.WriteLine("+----------+------------+");
+            for (int i = 0; i <= len - 1;i++)
+            {
+                Console.WriteLine("|{0, 5:d}     |  {1, 5:f2}     |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+------------+");
+            Console.ReadKey();
+        }
+    }
+}
